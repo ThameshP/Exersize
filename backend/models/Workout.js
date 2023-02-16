0cf3d46d2129c5e82501f683c6bbbc77960e5e5f
ThameshP <72529324+ThameshP@users.noreply.github.com>
2023-02-16 14:56:29 -0500

    schema

---
+++ backend/models/Workout.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+//We need to declare schema 
+const Schema = mongoose.Schema
+
+//Creates a new schema for us
+//Defines the structure of the workout collection
+const workoutSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true
+
+    },
+
+    rep: {
+        type: Number,
+        required: true
+    },
+
+    weight: {
+        type: Number,
+        required: true
+    },
+
+    note: {
+        type: String
+    } 
+
+},
+
+    {timestamps: true})
+
+
+//Model 
+
+module.exports = mongoose.model('Workout', workoutSchema)
