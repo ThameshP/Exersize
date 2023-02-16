6d66f3d83f29f822ae333d31d45910ba82c8f6d2
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-16 15:36:08 -0500

    Created main components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './Header'
+import Header from './Navbar'
 import Home from './Home';
 
 function App() {
---
+++ frontend/src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+        <h3>NAVBAR COMPONENT</h3>
+    </div>
+  )
+}
+
+export default Navbar
