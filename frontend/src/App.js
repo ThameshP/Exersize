21b9759a5337379c3556bcb10ad56371dfc44ff7
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-16 15:29:34 -0500

    Created main components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Header from './Header'
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
+    //BEM NAMING CONVENTION (names of components are lowercase)
+    <div className="app">
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ frontend/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div>
+        <h3>HEADER COMPONENT</h3>
+    </div>
+  )
+}
+
+export default Header
