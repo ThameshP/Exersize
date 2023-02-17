fdaa477432bb6fad1df68129f094df7f10f2f764
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-17 15:32:13 -0500

    Finished Part 9

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -3,7 +3,7 @@ import React from 'react'
 function WorkoutDetails({workout}) {
   return (
     <div className='workout-details'>
-        <h4>{WorkoutDetails.title}</h4>
+        <h4>{workout.title}</h4>
         <p><strong>Load (kg): </strong></p>
         <p><strong>Reps: </strong></p>
         <p>{workout.createdAt}</p>
